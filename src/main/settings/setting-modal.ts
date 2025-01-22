e32da583d219e2b900c96b4a3382913176af6228
Justin <chenyouduan@gmail.com>
2025-01-22 20:44:12 +0000

    refactor

--- src/main/settings/setting-modal.ts
+++ src/main/settings/setting-modal.ts
@@ -1,17 +1,11 @@
 export interface Settings {
-  source: {
-    translation: string
-    definition: string
-    example: string
-    kk: string
-  }
-  media: {
-    download: boolean
-    isNeed: boolean
-  }
-  limit: {
-    translation: number | null
-    definition: number | null
-    example: number | null
-  }
+  translation: string
+  definition: string
+  example: string
+  kk: string
+  download: boolean
+  isNeed: boolean
+  translationLimit: number | never
+  definitionLimit: number | never
+  exampleLimit: number | never
 }
