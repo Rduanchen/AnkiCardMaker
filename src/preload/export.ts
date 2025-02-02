7e7fa2979cfc90dcc8819a52798cecfd3c5a941a
Justin <chenyouduan@gmail.com>
2025-02-02 15:58:18 +0000

    feat: add exportSound method to export audio data for cards

--- src/preload/export.ts
+++ src/preload/export.ts
@@ -1,8 +1,10 @@
 import { ipcRenderer } from 'electron'
 const exportAPI = {
   exportCard: async (data: any) => {
-    console.log('preload exportCard')
     return await ipcRenderer.invoke('card:export-data', data)
+  },
+  exportSound: async (data: any) => {
+    return await ipcRenderer.invoke('card:export-sound', data)
   }
 }
 export default exportAPI
