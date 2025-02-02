ea7753ef1e77a156f8ae6dec059b8d1c3d557fc3
Justin <chenyouduan@gmail.com>
2025-02-02 15:59:42 +0000

    feat: enhance ApplicationSettings component with dynamic settings handling

--- src/renderer/src/components/ApplicationSettings.vue
+++ src/renderer/src/components/ApplicationSettings.vue
@@ -2,16 +2,19 @@
   <v-sheet class="pa-4">
     <v-form class="d-flex flex-column">
       <v-text class="mb-3">翻譯設定</v-text>
+      {{ settingsOptions }}
       <template v-for="option in settingsOptions">
         <v-select
           v-if="option.type === 'selection'"
           :label="option.name"
           :items="option.selections"
-          :value-field="option.default ? option.default : null"
+          v-model="onselectSettings[option.id]"
+          :value-field="option.default ? option.selections[option.default] : null"
         ></v-select>
         <v-text-field
           v-if="option.type === 'text'"
           :label="option.name"
+          v-model="onselectSettings[option.id]"
           :placeholder="option.placeholder ? option.placeholder : null"
         ></v-text-field>
       </template>
@@ -22,12 +25,16 @@
 export default {
   data() {
     return {
-      settingsOptions: {}
+      settingsOptions: {},
+      dictionariesSettings: {},
+      onselectSettings: {}
     }
   },
   async mounted() {
     try {
-      const response = await window.api.settings.settingOptions()
+      let response = await window.api.settings.settingOptions()
+      this.dictionariesSettings = response.dictionaries
+      delete response.dictionaries
       this.settingsOptions = response
     } catch (error) {
       console.error('Error fetching settings:', error)
