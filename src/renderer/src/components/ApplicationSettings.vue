c54558772468b2a106b36986d10b930bc7aa8f4d
Justin <chenyouduan@gmail.com>
2025-02-10 23:13:54 +0000

    feat: build up the settings info format.

--- src/renderer/src/components/ApplicationSettings.vue
+++ src/renderer/src/components/ApplicationSettings.vue
@@ -2,21 +2,25 @@
   <v-sheet class="pa-4">
     <v-form class="d-flex flex-column">
       <p class="mb-3">翻譯設定</p>
+      {{ translateSettings }}
       <v-select
         :label="dictionariesNames.name"
         :items="dictionariesNames.selections"
-        v-model="onselectDictionary"
+        v-model="dictionaryChange"
+        @click="changeDictionary"
       ></v-select>
       <template v-for="option in dictionarySettings[onselectDictionary]">
         <v-select
           v-if="option.type === 'selection'"
           :label="option.name"
           :items="option.selections"
+          v-model="translateSettings.dictionarySetting[option.id]"
           :value-field="option.default ? option.selections[option.default] : null"
         ></v-select>
         <v-text-field
           v-if="option.type === 'text'"
           :label="option.name"
+          v-model="translateSettings.dictionarySetting[option.id]"
           :placeholder="option.placeholder ? option.placeholder : null"
         ></v-text-field>
       </template>
@@ -24,6 +28,9 @@
   </v-sheet>
 </template>
 <script>
+import { mainStore } from '../store/main'
+import { mapWritableState } from 'pinia'
+
 export default {
   data() {
     return {
@@ -31,6 +38,18 @@ export default {
       systemSettings: {},
       dictionarySettings: {},
       onselectDictionary: ''
+    }
+  },
+  computed: {
+    ...mapWritableState(mainStore, ['translateSettings']),
+    dictionaryChange: {
+      get() {
+        return this.onselectDictionary
+      },
+      set(value) {
+        this.onselectDictionary = value
+        this.translateSettings.dictionaryName = value
+      }
     }
   },
   async mounted() {
@@ -41,6 +60,12 @@ export default {
       this.dictionarySettings = response.dictionaries
     } catch (error) {
       console.error('Error fetching settings:', error)
+    }
+  },
+  methods: {
+    changeDictionary() {
+      this.translateSettings.systemSettings = {}
+      this.translateSettings.dictionarySetting = {}
     }
   }
 }
