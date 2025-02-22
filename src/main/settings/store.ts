e78ed4057fc3730f1754860146e3fcd4da3df266
Justin <chenyouduan@gmail.com>
2025-02-22 23:15:01 +0000

    fix: add missing semicolons in settings store functions

--- src/main/settings/store.ts
+++ src/main/settings/store.ts
@@ -1,15 +1,16 @@
-import Store from 'electron-store'
-const store = new Store()
+import Store from 'electron-store';
+const store = new Store();
+
 function setDictionarySettings(data: any) {
-  store.set('settings:dictionarySettings', data)
+  store.set('settings:dictionarySettings', data);
 }
 
 function setChoesenDictionary(data: any) {
-  store.set('settings:choesenDictionary', data)
+  store.set('settings:choesenDictionary', data);
 }
 
 function setSystemSettings(data: any) {
-  store.set('settings:systemSettings', data)
+  store.set('settings:systemSettings', data);
 }
 
-export { setDictionarySettings, setChoesenDictionary, setSystemSettings }
+export { setDictionarySettings, setChoesenDictionary, setSystemSettings };
