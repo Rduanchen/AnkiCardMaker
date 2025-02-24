c417ecccae0ad4519b09e92dfa5b48714a41a944
Justin <chenyouduan@gmail.com>
2025-02-24 23:21:28 +0000

    feat: send setting value into clawer program

--- src/main/dictionarys/index.ts
+++ src/main/dictionarys/index.ts
@@ -1,37 +1,37 @@
 // const { ipcMain } = require('electron')
-import { ipcMain } from 'electron'
-import { CambridgeAutoComplete } from './cambridge/cambridge-auto-complete'
-import CambridgeClawer from './cambridge/cambridge-clawer'
-import pLimit from 'p-limit'
+import { ipcMain } from 'electron';
+import { CambridgeAutoComplete } from './cambridge/cambridge-auto-complete';
+import CambridgeClawer from './cambridge/cambridge-clawer';
+import pLimit from 'p-limit';
+import { getDictionarySettings } from '../settings/store';
 
-const limit = pLimit(5)
+const limit = pLimit(5);
 
 export default class DictionaryFunctions {
-  private cambridgeAutoComplete = new CambridgeAutoComplete()
+  private cambridgeAutoComplete = new CambridgeAutoComplete();
   constructor() {
-    this.setup()
+    this.setup();
   }
   private setup() {
-    console.log('DictionaryFunctions is ready')
     ipcMain.handle('dictionary:auto-complete', async (_event, word: string) => {
-      return await this.autoComplete(word)
-    })
+      return await this.autoComplete(word);
+    });
     ipcMain.handle('dictionary:search', async (_event, words: string[]) => {
-      const res = await this.dictionarySearch(words)
-      return res
-    })
+      const res = await this.dictionarySearch(words);
+      return res;
+    });
   }
   private autoComplete(word: string) {
-    return this.cambridgeAutoComplete.getAutoComplete(word)
+    return this.cambridgeAutoComplete.getAutoComplete(word);
   }
   private async dictionarySearch(words: string[]) {
     let allRequests = words.map((word) => {
       return limit(async () => {
-        let clawer = new CambridgeClawer(word)
-        await clawer.initialize()
-        return clawer.getDictionary()
-      })
-    })
-    return await Promise.all(allRequests)
+        let clawer = new CambridgeClawer(word, getDictionarySettings());
+        await clawer.initialize();
+        return clawer.getDictionary();
+      });
+    });
+    return await Promise.all(allRequests);
   }
 }
