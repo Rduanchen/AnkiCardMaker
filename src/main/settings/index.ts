8c3055356978d322a2df4171da87ee787b9c9314
Justin <chenyouduan@gmail.com>
2025-02-24 23:22:00 +0000

    refactor: rename setup method to IPCSetup for clarity and consistency

--- src/main/settings/index.ts
+++ src/main/settings/index.ts
@@ -1,12 +1,12 @@
 import { ipcMain } from 'electron';
 import dictionarySelection from './universal-settings';
 import CambridgeSetting from '../dictionarys/cambridge/cambridge-setting';
-import { setDictionarySettings, setChoesenDictionary, setSystemSettings } from './store';
+import { setDictionarySettings, setChosenDictionary, setSystemSettings } from './store.js';
 
 // Send the setting option to the renderer process.
 export default class SettingManager {
   constructor() {}
-  public setup() {
+  public IPCSetup() {
     ipcMain.handle('setting:get-options', async () => {
       const settingsResponse = {
         dictionaryNames: dictionarySelection,
@@ -19,10 +19,9 @@ export default class SettingManager {
     });
     ipcMain.handle('setting:set-options', async (_event, options: any) => {
       try {
-        console.log('Setting options:', options);
         if (options.dictionarySetting != undefined)
           setDictionarySettings(options.dictionarySetting);
-        if (options.dictionaryName != undefined) setChoesenDictionary(options.dictionaryName);
+        if (options.dictionaryName != undefined) setChosenDictionary(options.dictionaryName);
         if (options.systemSettings != undefined) setSystemSettings(options.systemSettings);
         return true;
       } catch (e) {
