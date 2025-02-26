9fec6de86c7f6f1a040757da814969c669bc337a
Justin <chenyouduan@gmail.com>
2025-02-26 12:33:05 +0000

    feat: add modifiedData property and update method for managing translations in SearchResultDisplay component

--- src/renderer/src/components/SearchResultDisplay.vue
+++ src/renderer/src/components/SearchResultDisplay.vue
@@ -1,25 +1,34 @@
 <template>
   <div class="search-result-display">
     <p>單字卡</p>
-    <v-btn @click="exportTest">輸出測試</v-btn>
+    {{ modifiedData }}
+    <v-btn @click="exportCardtoAnki">輸出</v-btn>
     <v-btn @click="exportAudioTest">音訊輸出測試</v-btn>
     <template v-for="(item, index) in dictionaries" :key="index">
-      <WordCard :wordDictionary="item" />
+      <WordCard
+        :info="item"
+        @dataChange="
+          (value) => {
+            this.updateTraslationData(index, value);
+          }
+        "
+      />
     </template>
   </div>
 </template>
 
 <script>
-import { mapWritableState } from 'pinia'
-import { mainStore } from '../store/main'
-import WordCard from './WordCard.vue'
-import emitter from '../mitt'
+import { mapWritableState } from 'pinia';
+import { mainStore } from '../store/main';
+import WordCard from './WordCard.vue';
+import emitter from '../mitt';
 export default {
   name: 'SearchResultDisplay',
   data() {
     return {
-      dictionaries: null
-    }
+      dictionaries: null,
+      modifiedData: undefined
+    };
   },
   components: {
     WordCard
@@ -28,29 +37,33 @@ export default {
     ...mapWritableState(mainStore, ['searchResult'])
   },
   methods: {
-    exportTest() {
+    updateTraslationData(index, value) {
+      this.modifiedData[index] = value;
+    },
+    exportCardtoAnki() {
       try {
-        let sendData = JSON.stringify(this.dictionaries)
+        let sendData = JSON.stringify(this.dictionaries);
         window.api.export.exportCard(sendData).then((result) => {
-          console.log('Export result:', result) // 確認回傳結果
-        })
+          console.log('Export result:', result); // 確認回傳結果
+        });
       } catch (error) {
-        console.error('Error exporting card:', error)
+        console.error('Error exporting card:', error);
       }
     },
     exportAudioTest() {
-      let sendData = JSON.stringify(this.dictionaries)
+      let sendData = JSON.stringify(this.dictionaries);
       window.api.export.exportSound(sendData).then((result) => {
-        console.log('Export result:', result) // 確認回傳結果
-      })
+        console.log('Export result:', result); // 確認回傳結果
+      });
     }
   },
   created() {
     emitter.on('searchResult', async (data) => {
-      this.dictionaries = await data
-    })
+      this.dictionaries = await data;
+      this.modifiedData = this.dictionaries;
+    });
   }
-}
+};
 </script>
 <style scoped lang="scss">
 .search-result-display {
