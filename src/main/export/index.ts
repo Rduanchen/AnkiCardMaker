8cf7e5be7e5b4ca710e59a2288485a6274035ffe
Justin <chenyouduan@gmail.com>
2025-02-26 12:32:29 +0000

    style: add missing semicolons for consistency and improve code readability

--- src/main/export/index.ts
+++ src/main/export/index.ts
@@ -1,43 +1,42 @@
-import { ipcMain } from 'electron'
-import { exportCard } from './process-data'
-import { saveTextToFile, selectFolder } from '../tools/save-file'
-import { downloadMp3Files } from '../tools/to-mp3'
-import { voiceDownloadModel } from '../tools/to-mp3'
+import { ipcMain } from 'electron';
+import { exportCard } from './process-data';
+import { saveTextToFile, selectFolder } from '../tools/save-file';
+import { downloadMp3Files } from '../tools/to-mp3';
+import { voiceDownloadModel } from '../tools/to-mp3';
 
 export default class MakeAnkiCard {
   constructor() {
-    this.setup()
+    this.setup();
   }
   private setup() {
     ipcMain.handle('card:export-data', async (_event, data) => {
-      return this.exportData(data)
-    })
+      return this.exportData(data);
+    });
     ipcMain.handle('card:export-sound', async (_event, data) => {
-      return this.exportSound(data)
-    })
+      return this.exportSound(data);
+    });
   }
   private exportData(data: any) {
-    console.log('exportData')
-    saveTextToFile(exportCard(data))
-    return ['success']
+    saveTextToFile(exportCard(data));
+    return ['success'];
   }
   private async exportSound(data: string) {
-    let datas = JSON.parse(data)
-    let downloadlist: voiceDownloadModel[] = []
+    let datas = JSON.parse(data);
+    let downloadlist: voiceDownloadModel[] = [];
     datas.forEach((item: any) => {
       downloadlist.push({
         url: item.audioURL.uk,
         fileName: item.volcabulary
-      } as voiceDownloadModel)
-    })
-    let filePath = await selectFolder()
+      } as voiceDownloadModel);
+    });
+    let filePath = await selectFolder();
     if (filePath) {
-      downloadMp3Files(downloadlist, filePath)
+      downloadMp3Files(downloadlist, filePath);
     } else {
-      console.error('No folder selected')
-      return ['error']
+      console.error('No folder selected');
+      return ['error'];
     }
-    console.log('exportSound')
-    return ['success']
+    console.log('exportSound');
+    return ['success'];
   }
 }
